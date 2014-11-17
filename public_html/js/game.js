34a785f9261323e2aa3c54d6016dc07a9c266f81
Raymundo Villegas <memovill@live.com>
2014-11-17 12:10:48 -0800

    new

--- public_html/js/game.js
+++ public_html/js/game.js
@@ -39,6 +39,8 @@ var game = {
 
 	// Run on game resources loaded.
 	"loaded" : function () {
+            me.pool.register("mario", game.playerEntinty, true);
+            
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
 
--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -8,7 +8,9 @@ game.PlayScreen = me.ScreenObject.extend({
                 
                 me.levelDirector.loadLevel("level04");
 
-		// add our HUD to the game world
+                var player = me.pool.pull("mario", 0, 420, {});
+                me.game.world.addChild(player, 3);
+            // add our HUD to the game world
 		this.HUD = new game.HUD.Container();
 		me.game.world.addChild(this.HUD);
 	},
