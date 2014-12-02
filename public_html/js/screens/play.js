687247fc8a0cd89df3931599acd0871966c4aa4c
Raymundo Villegas <memovill@live.com>
2014-12-02 10:38:56 -0800

    new

--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -8,7 +8,7 @@ game.PlayScreen = me.ScreenObject.extend({
                 
                 me.levelDirector.loadLevel("Raylevel01");
 
-               this.resetPlayer();
+               this.resetPlayer(0, 400);
                 
                 me.input.bindKey(me.input.KEY.RIGHT, "right");
                 
@@ -27,8 +27,8 @@ game.PlayScreen = me.ScreenObject.extend({
 		me.game.world.removeChild(this.HUD);
 	},
         
-        resetPlayer: function(){
-             var player = me.pool.pull("mario", 0, 420, {});
-              me.game.world.addChild(player, 3);
+        resetPlayer: function(x, y){
+             var player = me.pool.pull("mario", x, y, {});
+              me.game.world.addChild(player, 6);
         }
 });
